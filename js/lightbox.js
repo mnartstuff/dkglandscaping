a77faea3a301e8f6b3a131ac25df591a68ac5f30
mnartstuff <alexgeerartist@gmail.com>
2021-11-17 19:45:31 -0600

    JS- lightbox carousel and refactored code

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,23 +1,44 @@
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
+window.addEventListener('load', function(){
+  "use strict";
 
-const images = document.querySelectorAll('.lightbox-img');
-images.forEach((image) => {
-    image.addEventListener('click', () => {
-        lightbox.classList.add('active');
-        const img = document.createElement('img');
-        img.src = image.src;
+const images = document.querySelectorAll(".lightbox-img");
+const lightbox = document.querySelector('#lightbox');
+const lightboxImg = document.querySelector('#lightbox-img');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
 
-        while (lightbox.firstChild) {
-          lightbox.removeChild(lightbox.firstChild);
-        }
-        lightbox.appendChild(img);
+for( let i = 0; i < images.length; i++){
+  images[i].addEventListener('click', () => {
+    lightbox.classList.add('active');
+    let img = document.createElement('img');
+
+      img.src = images[i].src;
+      lightboxImg.appendChild(img);
+
+    lightbox.addEventListener('click', (e) => {
+        if (e.target !== e.currentTarget)
+          return;
+
+        img.remove();
+        lightbox.classList.remove('active');
       });
-  })
 
-lightbox.addEventListener('click', (e) => {
-    if (e.target !== e.currentTarget)
-      return;
-    lightbox.classList.remove('active');
-  })
+    prevBtn.addEventListener('click', (e)=> {
+      e.preventDefault();
+      i--;
+      if(i<0){i=images.length-1};
+      img.src = images[i].src;
+    });
+
+    nextBtn.addEventListener('click', (e)=> {
+      e.preventDefault();
+      i++;
+      if(i>=images.length) {i=0};
+      img.src = images[i].src;
+    });
+
+  });
+
+}
+
+});
